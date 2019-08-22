db610adbbfeae751447d5d4a8f6c7065cfa5b76d
Joonmyung <mytwiny@gmail.com>
2019-08-22 23:45:56 +0900

    Init

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Messio',
+      theme: ThemeData(
+        primarySwatch: Colors.yellow,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Messio'),
+      ),
+      body: Center(
+        child: Text('Hello World!')
+      ),
+    );
+  }
+}
